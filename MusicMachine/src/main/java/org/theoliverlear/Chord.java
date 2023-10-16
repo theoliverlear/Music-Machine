26197d6e9837772d4482ac06a30a737da1c8af90
theoliverlear <sigwartholiver2@gmail.com>
2023-10-16 13:59:13 -0500

    Add more chord identifications

--- MusicMachine/src/main/java/org/theoliverlear/Chord.java
+++ MusicMachine/src/main/java/org/theoliverlear/Chord.java
@@ -4,6 +4,7 @@ public class Chord {
     Note note1;
     Note note2;
     Note note3;
+    Note note4;
     String chordType;
     Chord(Note note1, Note note2, Note note3) {
         this.note1 = note1;
@@ -11,6 +12,22 @@ public class Chord {
         this.note3 = note3;
         this.determineChordType();
     }
+    Chord(Note note1, Note note2, Note note3, Note note4) {
+        this.note1 = note1;
+        this.note2 = note2;
+        this.note3 = note3;
+        this.note4 = note4;
+        this.determineChordType();
+    }
+    /*
+    1     0     1     1     0     0     0     1
+    128   64    32    16    8     4     2     1
+    128 + 0 + 32 + 16 + 0 + 0 + 0 + 1 = 177;
+
+    177
+
+    1 0 1 1 0 0 0 1
+     */
     //------------------------------Methods-----------------------------------
     public void determineChordType() {
         int rootToThird = this.note2.getNoteNumber() - this.note1.getNoteNumber();
@@ -21,6 +38,12 @@ public class Chord {
         } else if (rootToThird == Interval.MinorThird.getSemitonesFromRoot() &&
                 thirdToFifth == Interval.MajorThird.getSemitonesFromRoot()) {
             this.chordType = "Minor";
+        } else if (rootToThird == Interval.MajorThird.getSemitonesFromRoot() &&
+                thirdToFifth == Interval.MajorThird.getSemitonesFromRoot()) {
+            this.chordType = "Augmented";
+        } else if (rootToThird == Interval.MinorThird.getSemitonesFromRoot() &&
+        thirdToFifth == Interval.MinorThird.getSemitonesFromRoot()) {
+            this.chordType = "Diminished";
         } else {
             this.chordType = "Unknown";
         }
