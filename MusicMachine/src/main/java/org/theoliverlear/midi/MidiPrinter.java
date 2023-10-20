86c426235d348d5285085390ac80859924b9a02e
theoliverlear <sigwartholiver2@gmail.com>
2023-10-20 15:16:53 -0500

    Implement functionality that prints notes

--- MusicMachine/src/main/java/org/theoliverlear/midi/MidiPrinter.java
+++ MusicMachine/src/main/java/org/theoliverlear/midi/MidiPrinter.java
@@ -1,45 +1,88 @@
 package org.theoliverlear.midi;
 
+import org.theoliverlear.Note;
+
 import javax.sound.midi.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MidiPrinter {
     MidiDevice device;
     Receiver receiver;
     Transmitter transmitter;
+    String sharpOrFlat;
+    HashMap<Integer, Note> midiNoteMap = new HashMap<>();
     //----------------------------Constructors--------------------------------
     public MidiPrinter(MidiDevice device) throws MidiUnavailableException {
         this.device = device;
-        this.receiver = device.getReceiver();
         this.transmitter = device.getTransmitter();
-        this.transmitter.setReceiver(this.receiver);
-    }
-    //------------------------------Methods-----------------------------------
-    public void printMidiInput() throws MidiUnavailableException {
-        this.device.open();
-        this.device.close();
-    }
-    public void getMidiNote() throws MidiUnavailableException {
-        this.device.getTransmitter().setReceiver(new Receiver() {
+        this.sharpOrFlat = "Sharp";
+        ArrayList<Note> notes = new ArrayList<>(Arrays.asList(Note.values()));
+        System.out.println("Notes: " + notes);
+        for (Note note : notes) {
+            if (note.getNoteName().contains(this.sharpOrFlat) ||
+                    note.getNoteName().contains("Natural")) {
+                this.midiNoteMap.put(convertMidiNote(note.getNoteNumber()), note);
+            }
+        }
+        this.transmitter.setReceiver(new Receiver() {
             @Override
             public void send(MidiMessage message, long timeStamp) {
                 if (message instanceof ShortMessage) {
-                    ShortMessage noteMsg = (ShortMessage) message;
-                    if (noteMsg.getCommand() == ShortMessage.NOTE_ON) {
-                        System.out.println("Note on: " + noteMsg.getData1());
-                    } else if (noteMsg.getCommand() == ShortMessage.NOTE_OFF) {
-                        System.out.println("Note off: " + noteMsg.getData1());
+                    ShortMessage shortMessage = (ShortMessage) message;
+                    int key = shortMessage.getData1();
+                    int velocity = shortMessage.getData2();
+                    if (ShortMessage.NOTE_ON == shortMessage.getCommand() &&
+                            velocity > 0) {
+                        System.out.println("Key: " + midiNoteMap.get(key).getNoteName() + " Velocity: " + velocity);
                     }
                 }
             }
             @Override
-            public void close() {}
+            public void close() {
+                System.out.println("Receiver closed.");
+            }
         });
+
     }
-    public static void main(String[] args) throws MidiUnavailableException {
+    //------------------------------Methods-----------------------------------
+    public static int convertMidiNote(int midiKey) {
+        return midiKey + 21;
+    }
+    public void printMidiInput() throws MidiUnavailableException {
+        this.device.open();
+
+        this.device.close();
+    }
+    public ShortMessage getMidiNote() throws MidiUnavailableException {
+        ShortMessage midiNote = new ShortMessage();
+        return midiNote;
+    }
+    public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
         MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
-        MidiDevice device = MidiSystem.getMidiDevice(infos[5]);
+        //MidiDevice device = MidiSystem.getMidiDevice(infos[5]);
+        MidiDevice device = MidiSystem.getMidiDevice(infos[11]);
         MidiPrinter midiPrinter = new MidiPrinter(device);
-        midiPrinter.printMidiInput();
+        Thread thread = new Thread(() -> {
+            Scanner userInput = new Scanner(System.in);
+            String exit = "";
+            do {
+                System.out.println("Type \"exit\" to exit or press enter to continue.");
+                exit = userInput.next();
+                if (exit.contains("exit")) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+            } while (true);
+        });
+        midiPrinter.device.open();
+        thread.start();
+        thread.join();
+
+        midiPrinter.device.close();
+        //midiPrinter.printMidiInput();
     }
 }
