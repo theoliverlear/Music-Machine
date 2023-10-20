b6803d5b1c88b540bf690d4cd3ed789baaabeae5
theoliverlear <sigwartholiver2@gmail.com>
2023-10-20 15:15:33 -0500

    Replace variable names numbers with words

--- MusicMachine/src/main/java/org/theoliverlear/Chord.java
+++ MusicMachine/src/main/java/org/theoliverlear/Chord.java
@@ -1,39 +1,39 @@
 package org.theoliverlear;
 
 public class Chord {
-    Note note1;
-    Note note2;
-    Note note3;
-    Note note4;
+    Note noteOne;
+    Note noteTwo;
+    Note noteThree;
+    Note noteFour;
     String chordType;
     //----------------------------Constructors--------------------------------
-    public Chord(Note note1, Note note2, Note note3) {
-        this.note1 = note1;
-        this.note2 = note2;
-        this.note3 = note3;
+    public Chord(Note noteOne, Note noteTwo, Note noteThree) {
+        this.noteOne = noteOne;
+        this.noteTwo = noteTwo;
+        this.noteThree = noteThree;
         this.determineChordType();
     }
-    public Chord(Note note1, Note note2, Note note3, Note note4) {
-        this.note1 = note1;
-        this.note2 = note2;
-        this.note3 = note3;
-        this.note4 = note4;
+    public Chord(Note noteOne, Note noteTwo, Note noteThree, Note noteFour) {
+        this.noteOne = noteOne;
+        this.noteTwo = noteTwo;
+        this.noteThree = noteThree;
+        this.noteFour = noteFour;
         this.determineChordType();
     }
     //------------------------------Methods-----------------------------------
     public void determineChordType() {
-        int rootToThird = this.note2.getNoteNumber() -
-                          this.note1.getNoteNumber();
-        int thirdToFifth = this.note3.getNoteNumber() -
-                           this.note2.getNoteNumber();
+        int rootToThird = this.noteTwo.getNoteNumber() -
+                          this.noteOne.getNoteNumber();
+        int thirdToFifth = this.noteThree.getNoteNumber() -
+                           this.noteTwo.getNoteNumber();
         int rootToSeventh = 0;
-        if (note4 != null) {
-            rootToSeventh = this.note4.getNoteNumber() -
-                            this.note1.getNoteNumber();
+        if (noteFour != null) {
+            rootToSeventh = this.noteFour.getNoteNumber() -
+                            this.noteOne.getNoteNumber();
         }
         if (rootToThird == Interval.MajorThird.getSemitonesFromRoot() &&
                 thirdToFifth == Interval.MinorThird.getSemitonesFromRoot()) {
-            if (note4 == null) {
+            if (noteFour == null) {
                 this.chordType = "Major";
             } else if (rootToSeventh == Interval.MajorSeventh.getSemitonesFromRoot()) {
                 this.chordType = "Major 7th";
@@ -42,7 +42,7 @@ public class Chord {
             }
         } else if (rootToThird == Interval.MinorThird.getSemitonesFromRoot() &&
                 thirdToFifth == Interval.MajorThird.getSemitonesFromRoot()) {
-            if (note4 == null) {
+            if (noteFour == null) {
                 this.chordType = "Minor";
             } else if (rootToSeventh == Interval.MajorSeventh.getSemitonesFromRoot()) {
                 this.chordType = "Minor Major 7th";
@@ -60,33 +60,33 @@ public class Chord {
         }
     }
     //------------------------------Getters-----------------------------------
-    public Note getNote1() {
-        return this.note1;
+    public Note getNoteOne() {
+        return this.noteOne;
     }
-    public Note getNote2() {
-        return this.note2;
+    public Note getNoteTwo() {
+        return this.noteTwo;
     }
-    public Note getNote3() {
-        return this.note3;
+    public Note getNoteThree() {
+        return this.noteThree;
     }
-    public Note getNote4() {
-        return this.note4;
+    public Note getNoteFour() {
+        return this.noteFour;
     }
     public String getChordType() {
         return this.chordType;
     }
     //------------------------------Setters-----------------------------------
-    public void setNote1(Note note1) {
-        this.note1 = note1;
+    public void setNoteOne(Note noteOne) {
+        this.noteOne = noteOne;
     }
-    public void setNote2(Note note2) {
-        this.note2 = note2;
+    public void setNoteTwo(Note noteTwo) {
+        this.noteTwo = noteTwo;
     }
-    public void setNote3(Note note3) {
-        this.note3 = note3;
+    public void setNoteThree(Note noteThree) {
+        this.noteThree = noteThree;
     }
-    public void setNote4(Note note4) {
-        this.note4 = note4;
+    public void setNoteFour(Note noteFour) {
+        this.noteFour = noteFour;
     }
     public void setChordType(String chordType) {
         this.chordType = chordType;
