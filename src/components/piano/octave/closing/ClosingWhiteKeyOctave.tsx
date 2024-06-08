cf70d448e93d9b33aa477e24f01ca679c82acf58
theoliverlear <sigwartholiver2@gmail.com>
2024-06-08 14:58:20 -0500

    Add ClosingWhiteKeyOctave.tsx for rendering closing octave white keys on piano

---
+++ src/components/piano/octave/closing/ClosingWhiteKeyOctave.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ClosingWhiteKeyOctave() {
+    return (
+        <div className="white-keys-octave key-octave">
+            <div className="key white-key" data-note="C8 | B#7"></div>
+        </div>
+    );
+}
+
+export default ClosingWhiteKeyOctave;
