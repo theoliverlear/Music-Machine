3e176a85a1777b817ca80c05fc1e084747aa77d7
theoliverlear <sigwartholiver2@gmail.com>
2024-06-08 15:02:24 -0500

    Add OpeningBlackKeyOctave.tsx to create the opening black key octave component for the piano

---
+++ src/components/piano/octave/opening/OpeningBlackKeyOctave.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OpeningBlackKeyOctave() {
+    return (
+        <div className="black-keys-octave key-octave">
+            <div className="key black-key" data-note="A#0 | Bb0"></div>
+        </div>
+    );
+}
+
+export default OpeningBlackKeyOctave;
