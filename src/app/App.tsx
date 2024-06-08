2caed42e99b38ef4a0cc7e4f699f62194097ad26
theoliverlear <sigwartholiver2@gmail.com>
2024-06-08 14:56:54 -0500

    Add App.tsx for initializing the application functionality routing

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FreePlay from "../pages/FreePlay";
+
+function App() {
+  return (
+    <div className="App">
+        <FreePlay />
+    </div>
+  );
+}
+
+export default App;
