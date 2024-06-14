999a998a3bde5e09878508ea10da8bd7291a498a
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 18:42:41 -0500

    Refactor MenuItem.tsx to include Title component and add click handler functionality

--- src/components/ui/menu/MenuItem.tsx
+++ src/components/ui/menu/MenuItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Title from "../../title/Title";
 
-function MenuItem(props: { title: string }) {
+function MenuItem(props: { title: string, titleSize: number, onMenuItemClick?: (title: string) => void }) {
     return (
         <div className="menu-item-div">
-            <h3>{props.title}</h3>
+            <Title title={props.title} size={props.titleSize} onClick={props.onMenuItemClick} />
         </div>
     );
 }
+export default MenuItem;
