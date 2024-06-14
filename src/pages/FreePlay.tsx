fd6ad10d5eed00919f0753fb8cefc5d5ad725a80
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 18:40:39 -0500

    Add MidiPopup component in FreePlay.tsx replacing the MidiController component

--- src/pages/FreePlay.tsx
+++ src/pages/FreePlay.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Piano from "../components/piano/Piano";
 import MidiController from "../components/midi/MidiController";
 import Title from "../components/title/Title";
+import MidiPopup from "../components/midi/MidiPopup";
 
 function FreePlay() {
     return (
         <div>
             <Title title="Free Play" size={1}/>
-            <MidiController />
+            <MidiPopup />
             <Piano />
         </div>
     );
