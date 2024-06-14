6e642d454bad8201e0d0b9b9a17d6e6ffc31e23e
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 18:34:07 -0500

    Add routing to Home and FreePlay pages and introduce Router in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import FreePlay from "../pages/FreePlay";
+import Home from "../pages/Home";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+const menuClassName: string = "menu-item";
 
 function App() {
   return (
-    <div className="App">
-        <FreePlay />
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home className={menuClassName} />} />
+              <Route path="/free-play" element={<FreePlay />} />
+          </Routes>
+      </Router>
   );
 }
 
