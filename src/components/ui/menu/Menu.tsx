14af1524cebfb33a6c498523718cdea1eead37e2
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 18:42:21 -0500

    Refactor Menu.tsx to use MenuItem component and include additional menu title, size, and onClick properties

--- src/components/ui/menu/Menu.tsx
+++ src/components/ui/menu/Menu.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Title from "../../title/Title";
+import MenuItem from "./MenuItem";
+import './Menu.css';
 
-function Menu(props: {title: string, listItemsTitles: string[], listItemSize: number}) {
+function Menu(props: {title: string,
+    titleSize: number,
+    listItemsTitles: string[],
+    listItemSize: number,
+    onMenuItemClick?: (title: string) => void}) {
     return (
         <div className="menu-div">
-            <Title title={props.title} size={1}/>
+            <Title title={props.title} size={props.titleSize}/>
             <div className="menu-items-div">
-                {props.listItemsTitles.map((title, index) => {
-                    return <div key={index} className="menu-item-div">
-                        <Title title={title} size={props.listItemSize}/>
-                    </div>
-                })}
+                {props.listItemsTitles.map((listItemTitle: string) => <MenuItem title={listItemTitle} titleSize={props.listItemSize} onMenuItemClick={props.onMenuItemClick}/>)}
             </div>
         </div>
     );
 }
+export default Menu;
