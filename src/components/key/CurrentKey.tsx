17d48163692dd51ca7bb2f3c30e44ee5d07d7f71
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 18:39:35 -0500

    Add CurrentKey.tsx to allow for key or mode selection and display roman numeral notation for chords and scales

---
+++ src/components/key/CurrentKey.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CurrentKey() {
+    return (
+        <div>
+            <h1>Current Key</h1>
+        </div>
+    );
+}
+export default CurrentKey;
+// Allow for choosing a key or mode someone is in or have it auto-detected.
+// This can be used to show roman numeral notation for chords and scales.
