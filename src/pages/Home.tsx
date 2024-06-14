23c787474582be8b99190cbb0680b9ea83ad450d
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 18:41:43 -0500

    Refactor Home.tsx to include new Title component and additional component links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import Title from "../components/title/Title";
-function Home() {
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import FreePlay from "./FreePlay";
+function Home(props: {className: string}) {
     return (
-        <div>
-            <Title title="Free Play" size={1}/>
+        <div className="home">
+            <Title title="Music Machine" size={1}/>
+            <hr />
+            <div className={props.className}>
+                <Title title="Learn Songs" size={3} />
+            </div>
+            <div className={props.className}>
+                <Title title="Music Theory Engine" size={3} />
+            </div>
+            <div className={props.className}>
+                <Link to={"/free-play"} className="link-no-style">
+                    <Title title="Free Play" size={3} />
+                </Link>
+            </div>
         </div>
     );
 }
+export default Home;
