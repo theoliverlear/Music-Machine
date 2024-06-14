77fdef69814cd8216487c51ad08535d71ca60ad4
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 09:54:28 -0500

    Add Home.tsx to create a home page with "Free Play" as a title

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Title from "../components/title/Title";
+function Home() {
+    return (
+        <div>
+            <Title title="Free Play" size={1}/>
+        </div>
+    );
+}
