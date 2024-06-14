f129504fb101938f533efb6a7c17fecc1264c526
theoliverlear <sigwartholiver2@gmail.com>
2024-06-14 09:56:34 -0500

    Add MidiController component and Title in FreePlay.tsx

--- src/pages/FreePlay.tsx
+++ src/pages/FreePlay.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Piano from "../components/piano/Piano";
+import MidiController from "../components/midi/MidiController";
+import Title from "../components/title/Title";
 
 function FreePlay() {
     return (
         <div>
+            <Title title="Free Play" size={1}/>
+            <MidiController />
             <Piano />
         </div>
     );
