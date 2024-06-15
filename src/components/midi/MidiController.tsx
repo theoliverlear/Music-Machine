48353cb92ad557a603a2a5015de76a41a90b7341
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 12:12:29 -0500

    Refactor MidiController.tsx to include MidiPopup and MidiDevice components and handle MIDI device selection

--- src/components/midi/MidiController.tsx
+++ src/components/midi/MidiController.tsx
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
+import MidiPopup from "./MidiPopup";
+import MidiDevice from "./MidiDevice";
 
 function MidiController() {
+    const [midiInput, setMidiInput] = useState<WebMidi.MIDIInput | undefined>(undefined);
+    const [midiOutput, setMidiOutput] = useState<WebMidi.MIDIOutput | undefined>(undefined);
+    function handleMidiDeviceSelection(inputDevice: WebMidi.MIDIInput | undefined, outputDevice: WebMidi.MIDIOutput | undefined) {
+        console.log('Midi Device Selected');
+        setMidiInput(inputDevice);
+        setMidiOutput(outputDevice);
+    }
     return (
         <div>
-
+            <MidiPopup onMidiDeviceSelected={handleMidiDeviceSelection} />
+            <MidiDevice input={midiInput} output={midiOutput} />
         </div>
     )
 }
