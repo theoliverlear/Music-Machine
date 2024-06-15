b96f05f0d0e8cd1538416876cad51e2ff48928c7
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 16:08:01 -0500

    Refactor Piano.tsx to highlight notes currently being played

--- src/components/piano/Piano.tsx
+++ src/components/piano/Piano.tsx
@@ -3,24 +3,30 @@ import OpeningOctave from "./octave/opening/OpeningOctave";
 import KeyOctave from "./octave/KeyOctave";
 import ClosingOctave from "./octave/closing/ClosingOctave";
 import './Piano.css';
+import {Note} from "../../models/Note";
 
 interface PianoProps {
-    //onMidiMessage: (message: WebMidi.MIDIMessageEvent) => void;
+    currentNotes: Note[];
 }
 
 function Piano(props: PianoProps) {
+    function isNotePlayed(dataNote: string): boolean {
+        const [firstNote, secondNote] = dataNote.split(" | ");
+        let isPlayed = props.currentNotes.some(note => note.noteData.fullNoteName === firstNote || note.noteData.fullNoteName === secondNote);
+        return isPlayed;
+    }
     return (
         <div className="piano-container">
             <div className="key-container">
-                <OpeningOctave />
-                <KeyOctave octaveNumber={1} />
-                <KeyOctave octaveNumber={2} />
-                <KeyOctave octaveNumber={3} />
-                <KeyOctave octaveNumber={4} />
-                <KeyOctave octaveNumber={5} />
-                <KeyOctave octaveNumber={6} />
-                <KeyOctave octaveNumber={7} />
-                <ClosingOctave />
+                <OpeningOctave currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={1}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={2}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={3}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={4}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={5}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={6}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <KeyOctave octaveNumber={7}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
+                <ClosingOctave currentNotes={props.currentNotes} isNotePlayed={isNotePlayed} />
             </div>
         </div>
     );
