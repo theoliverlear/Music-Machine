3f8da3b33e52955dbfefcc88a65baf78cb8bb700
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 16:05:35 -0500

    Refactor OpeningBlackKeyOctave.tsx to add currentNotes prop and highlight pressed keys

--- src/components/piano/octave/opening/OpeningBlackKeyOctave.tsx
+++ src/components/piano/octave/opening/OpeningBlackKeyOctave.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Note} from "../../../../models/Note";
 
-function OpeningBlackKeyOctave() {
+interface OpeningBlackKeyOctaveProps {
+    currentNotes: Note[];
+    isNotePlayed: (dataNote: string) => boolean;
+}
+
+function OpeningBlackKeyOctave(props: OpeningBlackKeyOctaveProps) {
+    function getClassStyle(dataNote: string): string {
+        return `key black-key ${props.isNotePlayed(dataNote) ? "black-key-pressed" : ""}`;
+    }
     return (
         <div className="black-keys-octave key-octave">
-            <div className="key black-key" data-note="A#0 | Bb0"></div>
+            <div className={getClassStyle("A#0 | Bb0")} data-note="A#0 | Bb0"></div>
         </div>
     );
 }
