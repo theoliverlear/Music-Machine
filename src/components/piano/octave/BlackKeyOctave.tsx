1ed08207f67ecc824048808688916d87ff6738a3
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 15:57:10 -0500

    Refactor BlackKeyOctave.tsx to include note played check and dynamic class styles

--- src/components/piano/octave/BlackKeyOctave.tsx
+++ src/components/piano/octave/BlackKeyOctave.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import {Note} from "../../../models/Note";
 
-function BlackKeyOctave(props: { octaveNumber: number }) {
+interface BlackKeyOctaveProps {
+    octaveNumber: number;
+    currentNotes: Note[];
+    isNotePlayed: (dataNote: string) => boolean;
+}
+
+function BlackKeyOctave(props: BlackKeyOctaveProps) {
+    function getClassStyle(dataNote: string): string {
+        return `key black-key ${props.isNotePlayed(dataNote) ? "black-key-pressed" : ""}`;
+    }
     return (
-    <div className="black-keys-octave key-octave">
-        <div className="key black-key" data-note={`C#${props.octaveNumber} | Db${props.octaveNumber}`}></div>
-        <div className="key black-key" data-note={`D#${props.octaveNumber} | Eb${props.octaveNumber}`}></div>
-        <div className="key black-key" data-note={`F#${props.octaveNumber} | Gb${props.octaveNumber}`}></div>
-        <div className="key black-key" data-note={`G#${props.octaveNumber} | Ab${props.octaveNumber}`}></div>
-        <div className="key black-key" data-note={`A#${props.octaveNumber} | Bb${props.octaveNumber}`}></div>
-    </div>
+        <div className="black-keys-octave key-octave">
+            <div className={getClassStyle(`C#${props.octaveNumber} | Db${props.octaveNumber}`)} data-note={`C#${props.octaveNumber} | Db${props.octaveNumber}`}></div>
+            <div className={getClassStyle(`D#${props.octaveNumber} | Eb${props.octaveNumber}`)} data-note={`D#${props.octaveNumber} | Eb${props.octaveNumber}`}></div>
+            <div className={getClassStyle(`F#${props.octaveNumber} | Gb${props.octaveNumber}`)} data-note={`F#${props.octaveNumber} | Gb${props.octaveNumber}`}></div>
+            <div className={getClassStyle(`G#${props.octaveNumber} | Ab${props.octaveNumber}`)} data-note={`G#${props.octaveNumber} | Ab${props.octaveNumber}`}></div>
+            <div className={getClassStyle(`A#${props.octaveNumber} | Bb${props.octaveNumber}`)} data-note={`A#${props.octaveNumber} | Bb${props.octaveNumber}`}></div>
+        </div>
     );
 }
 
