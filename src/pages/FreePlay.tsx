d551b603ba8bba4e2f561ae5db84fc9fa35fa8d0
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 12:11:42 -0500

    Add midiInput state and handleMidiMessage function in FreePlay.tsx

--- src/pages/FreePlay.tsx
+++ src/pages/FreePlay.tsx
@@ -4,11 +4,17 @@ import MidiController from "../components/midi/MidiController";
 import Title from "../components/title/Title";
 import MidiPopup from "../components/midi/MidiPopup";
 
+
 function FreePlay() {
+    const [midiInput, setMidiInput] = React.useState<WebMidi.MIDIInput | undefined>(undefined);
+    function handleMidiMessage(message: WebMidi.MIDIMessageEvent) {
+        console.log('Midi Message Received');
+
+    }
     return (
         <div>
             <Title title="Free Play" size={1}/>
-            <MidiPopup />
+            <MidiController />
             <Piano />
         </div>
     );
