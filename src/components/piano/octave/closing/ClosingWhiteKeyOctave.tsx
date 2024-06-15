465876eae92cb0dccbcfebc0a2234481fad694a5
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 15:58:13 -0500

    Refactor ClosingWhiteKeyOctave.tsx to include note prop and implement dynamic key styling

--- src/components/piano/octave/closing/ClosingWhiteKeyOctave.tsx
+++ src/components/piano/octave/closing/ClosingWhiteKeyOctave.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Note} from "../../../../models/Note";
 
-function ClosingWhiteKeyOctave() {
+interface ClosingWhiteKeyOctaveProps {
+    currentNotes: Note[];
+    isNotePlayed: (dataNote: string) => boolean;
+}
+
+function ClosingWhiteKeyOctave(props: ClosingWhiteKeyOctaveProps) {
+    function getClassStyle(dataNote: string): string {
+        return `key white-key ${props.isNotePlayed(dataNote) ? "white-key-pressed" : ""}`;
+    }
     return (
         <div className="white-keys-octave key-octave">
-            <div className="key white-key" data-note="C8 | B#7"></div>
+            <div className={getClassStyle("C8 | B#7")} data-note="C8 | B#7"></div>
         </div>
     );
 }
