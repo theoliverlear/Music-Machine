0dac3af49d29ceff416b8ef56993e1a7ca4d5782
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 16:05:53 -0500

    Refactor OpeningOctave.tsx to include props for currentNotes and isNotePlayed in Piano component

--- src/components/piano/octave/opening/OpeningOctave.tsx
+++ src/components/piano/octave/opening/OpeningOctave.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import OpeningWhiteKeyOctave from "./OpeningWhiteKeyOctave";
 import OpeningBlackKeyOctave from "./OpeningBlackKeyOctave";
+import {Note} from "../../../../models/Note";
 
-function OpeningOctave() {
+interface OpeningOctaveProps {
+    currentNotes: Note[];
+    isNotePlayed: (dataNote: string) => boolean;
+}
+
+function OpeningOctave(props: OpeningOctaveProps) {
     return (
         <div className={"octave-container opening-octave"}>
-            <OpeningWhiteKeyOctave />
-            <OpeningBlackKeyOctave />
+            <OpeningWhiteKeyOctave currentNotes={props.currentNotes} isNotePlayed={props.isNotePlayed}/>
+            <OpeningBlackKeyOctave currentNotes={props.currentNotes} isNotePlayed={props.isNotePlayed}/>
         </div>
     );
 }
