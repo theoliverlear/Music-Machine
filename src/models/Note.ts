2c61494822a0fa094d6986434824a0673fdd9a68
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 16:04:53 -0500

    Add functionality to Note.ts for connecting note data with midi notes

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -1,3 +1,34 @@
-export class Note {
+import {MidiNote} from "./MidiNote";
+import {NoteData} from "./NoteData";
 
+export class Note {
+    private _midiNote: MidiNote;
+    private _noteNumber: number;
+    private _noteData: NoteData;
+    constructor(midiNote: MidiNote = new MidiNote()) {
+        this._midiNote = midiNote;
+        this._noteNumber = midiNote.noteId - 21;
+        this._noteData = NoteData.getByNoteNumber(this._noteNumber);
+    }
+    get midiNote(): MidiNote {
+        return this._midiNote;
+    }
+    get noteNumber(): number {
+        return this._noteNumber;
+    }
+    get noteData(): NoteData {
+        return this._noteData;
+    }
+    setMidiNote(midiNote: MidiNote): void {
+        this._midiNote = midiNote;
+    }
+    setNoteNumber(noteNumber: number): void {
+        this._noteNumber = noteNumber;
+    }
+    setNoteData(noteData: NoteData): void {
+        this._noteData = noteData;
+    }
+    toString(): string {
+        return `Note{midiNote: ${this._midiNote}, noteNumber: ${this._noteNumber}, noteName: ${this._noteData.fullNoteName}\n`;
+    }
 }
