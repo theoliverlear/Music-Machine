be1f1632c53ed3663982343b2e1808c930258cf3
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 16:02:56 -0500

    Refactor KeyOctave.tsx to include currentNotes and isNotePlayed props in piano component

--- src/components/piano/octave/KeyOctave.tsx
+++ src/components/piano/octave/KeyOctave.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import WhiteKeyOctave from "./WhiteKeyOctave";
 import BlackKeyOctave from "./BlackKeyOctave";
+import {Note} from "../../../models/Note";
+import currentNotes from "../../music_display/note/CurrentNotes";
 
-function KeyOctave(props: { octaveNumber: number }) {
+interface KeyOctaveProps {
+    octaveNumber: number;
+    currentNotes: Note[];
+    isNotePlayed: (dataNote: string) => boolean;
+}
+
+function KeyOctave(props: KeyOctaveProps) {
     return (
         <div className="octave-container">
-            <WhiteKeyOctave octaveNumber={props.octaveNumber}/>
-            <BlackKeyOctave octaveNumber={props.octaveNumber}/>
+            <WhiteKeyOctave octaveNumber={props.octaveNumber} currentNotes={props.currentNotes} isNotePlayed={props.isNotePlayed}/>
+            <BlackKeyOctave octaveNumber={props.octaveNumber} currentNotes={props.currentNotes} isNotePlayed={props.isNotePlayed}/>
         </div>
     );
 }
