739e5d4dc3ce327172fecd76113a77fb3d29d8ab
theoliverlear <sigwartholiver2@gmail.com>
2024-06-15 20:12:16 -0500

    Add implementation and fields to Chord class in Chord.ts

--- src/models/Chord.ts
+++ src/models/Chord.ts
@@ -1,3 +1,27 @@
-export class Chord {
+import {Note} from "./Note";
+import {ChordInterval} from "./ChordInterval";
+import {Interval} from "./Interval";
 
+export class Chord {
+    private _name: string;
+    private _notes: Note[];
+    constructor(name: string = "", notes: Note[] = []) {
+        this._notes = notes;
+        this._name = this.determineChordName();
+    }
+    determineChordName(): string {
+        let chordIntervals: Interval[] = [];
+        chordIntervals.push(Interval.unison);
+        for (let i: number = 0; i < this._notes.length - 1; i++) {
+            chordIntervals.push(Interval.getIntervalBetweenNotes(this._notes[i], this._notes[i + 1]));
+        }
+        let chordInterval: ChordInterval = new ChordInterval(chordIntervals);
+        return chordInterval.getNameByIntervals();
+    }
+    get name(): string {
+        return this._name;
+    }
+    get notes(): Note[] {
+        return this._notes;
+    }
 }
