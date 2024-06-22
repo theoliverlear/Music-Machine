2e3dde150150d8efdef7ce6ed775260c1b9b38b3
theoliverlear <sigwartholiver2@gmail.com>
2024-06-22 12:39:13 -0500

    Add MusicSet.ts model file to manage music items holding unique values

---
+++ src/models/MusicSet.ts
@@ -0,0 +1,59 @@
+import {Equatable} from "./Equatable";
+
+export class MusicSet<T extends Equatable> {
+    private _musicItems: Set<T>;
+    constructor(musicItems: T[] = []) {
+        this._musicItems = new Set<T>();
+        this.addAll(musicItems);
+    }
+    contains(musicItem: T): boolean {
+        let containsItem: boolean = false;
+        for (let currentMusicItem of this._musicItems) {
+            if (currentMusicItem.equals(musicItem)) {
+                containsItem = true;
+                break;
+            }
+        }
+        return containsItem;
+    }
+    add(musicItem: T): void {
+        if (!this.contains(musicItem)) {
+            this._musicItems.add(musicItem);
+        }
+    }
+    addAll(musicItems: T[]): void {
+        for (let musicItem of musicItems) {
+            if (!this.contains(musicItem)) {
+                this._musicItems.add(musicItem);
+            }
+        }
+    }
+    updateAll(musicItems: T[]): void {
+        this._musicItems.clear();
+        this.addAll(musicItems);
+    }
+    remove(musicItem: T): void {
+        if (this.contains(musicItem)) {
+            this._musicItems.delete(musicItem);
+        }
+    }
+    removeAll(musicItems: T[]): void {
+        for (let musicItem of musicItems) {
+            if (this.contains(musicItem)) {
+                this._musicItems.delete(musicItem);
+            }
+        }
+    }
+    get musicItems(): Set<T> {
+        return this._musicItems;
+    }
+    get size(): number {
+        return this._musicItems.size;
+    }
+    get notesArray(): T[] {
+        return Array.from(this._musicItems);
+    }
+    setNotes(notes: Set<T>): void {
+        this._musicItems = new Set<T>(notes);
+    }
+}
