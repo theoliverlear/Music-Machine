1015597c87a2a6476c957732d5c8e104e494461e
theoliverlear <sigwartholiver2@gmail.com>
2025-06-07 19:44:05 -0500

    Refactor Menu.tsx to introduce `ReactElement` return type and use interface for props

--- src/components/elements/element-group-menu/menu/Menu.tsx
+++ src/components/elements/element-group-menu/menu/Menu.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import Title from "../../element-group-native/title/Title";
 import MenuItem from "../menu-item/MenuItem";
 import './Menu.css';
+import {TagType} from "../../../../models/html/TagType";
 
-function Menu(props: {title: string,
-    titleSize: number,
-    listItemsTitles: string[],
-    listItemSize: number,
-    onMenuItemClick?: (title: string) => void}) {
+interface MenuProps {
+    title: string;
+    tagType: TagType;
+    listItemsTitles: string[];
+    listItemTagType: TagType;
+    onMenuItemClick?: (title: string) => void;
+}
+
+function Menu(props: MenuProps): ReactElement {
     return (
         <div className="menu-div">
-            <Title title={props.title} size={props.titleSize}/>
+            <Title text={props.title} tagType={props.tagType}/>
             <div className="menu-items-div">
-                {props.listItemsTitles.map((listItemTitle: string) => <MenuItem title={listItemTitle} titleSize={props.listItemSize} onMenuItemClick={props.onMenuItemClick}/>)}
+                {props.listItemsTitles.map((listItemTitle: string) => {
+                    return <MenuItem text={listItemTitle}
+                                     tagType={props.listItemTagType}
+                                     onMenuItemClick={props.onMenuItemClick}/>;
+                })}
             </div>
         </div>
     );
