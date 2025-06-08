f1a8b9c9e4f713371e1c785636b6ef7aafe16507
theoliverlear <sigwartholiver2@gmail.com>
2025-06-07 19:49:39 -0500

    Refactor CurrentNotes.tsx to use `ReactElement` return type, `TagType` enum, and updated `Title` prop names

--- src/components/elements/element-group-note/current-notes/CurrentNotes.tsx
+++ src/components/elements/element-group-note/current-notes/CurrentNotes.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {Note} from "../../../../models/note/Note";
-import {MidiNote} from "../../../../models/midi/MidiNote";
 import CurrentNote from "../current-note/CurrentNote";
 import Title from "../../element-group-native/title/Title";
 import './CurrentNotes.css';
+import {TagType} from "../../../../models/html/TagType";
 
 interface CurrentNotesProps {
     currentNotes: Note[];
 }
 
-function CurrentNotes(props: CurrentNotesProps) {
+function CurrentNotes(props: CurrentNotesProps): ReactElement {
     return (
         <div className="title-with-notes">
-            <Title title="Current Notes: " size={4} />
+            <Title text="Current Notes: " tagType={TagType.H4} />
+            {props.currentNotes.length === 0 && <Title text={"None"} tagType={TagType.H5}/>}
             <div className="current-notes">
-                {/*<CurrentNote currentNote={new Note(new MidiNote(50))} />*/}
-                {/*<CurrentNote currentNote={new Note(new MidiNote(52))} />*/}
-                {props.currentNotes.map((note, index) => <CurrentNote key={index} currentNote={note} />)}
+                {props.currentNotes.map((note, index) => <CurrentNote key={index} currentNote={note}/>)}
             </div>
         </div>
     );
