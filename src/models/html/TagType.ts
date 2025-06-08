7a9ae856294a3820e9f23ef6da2f3ed71f2b0026
theoliverlear <sigwartholiver2@gmail.com>
2025-06-07 19:45:35 -0500

    Add TagType.ts to define an enum for HTML tag types

---
+++ src/models/html/TagType.ts
@@ -0,0 +1,12 @@
+export enum TagType {
+    H1 = 'h1',
+    H2 = 'h2',
+    H3 = 'h3',
+    H4 = 'h4',
+    H5 = 'h5',
+    H6 = 'h6',
+    P = 'p',
+    SPAN = 'span',
+    DIV = 'div',
+    CODE = 'code',
+}
