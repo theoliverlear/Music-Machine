0536992560126db87a729e5ec0e8a9a87b218206
theoliverlear <sigwartholiver2@gmail.com>
2025-06-08 11:02:58 -0500

    Refactor Home.tsx to simplify prop handling

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -4,25 +4,22 @@ import {Link} from "react-router-dom";
 import Title from "../../elements/element-group-native/title/Title";
 import {TagType} from "../../../models/html/TagType";
 
-interface HomeProps {
-    className: string;
-}
 
-function Home(props: HomeProps): ReactElement {
+function Home(): ReactElement {
     return (
         <div className="home">
             <Title text="Music Machine" tagType={TagType.H1}/>
-            <hr />
-            <div className={props.className}>
-                <Title text="Learn Songs" tagType={TagType.H3} />
-            </div>
-            <div className={props.className}>
-                <Title text="Music Theory Engine" tagType={TagType.H3} />
-            </div>
-            <div className={props.className}>
+            <hr/>
+            <div className={"menu-item"}>
                 <Link to={"/free-play"} className="link-no-style">
                     <Title text="Free Play" tagType={TagType.H3} />
                 </Link>
+            </div>
+            <div className={"menu-item"}>
+                <Title text="Learn Songs" tagType={TagType.H3} />
+            </div>
+            <div className={"menu-item"}>
+                <Title text="Music Theory Engine" tagType={TagType.H3} />
             </div>
         </div>
     );
