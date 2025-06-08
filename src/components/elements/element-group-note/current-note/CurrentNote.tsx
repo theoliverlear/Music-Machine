ab2292526e5f3d3276632eedd5649f20adef871c
theoliverlear <sigwartholiver2@gmail.com>
2025-06-07 19:49:14 -0500

    Refactor CurrentNote.tsx to use `ReactElement` return type, `TagType` enum, and updated `Title` prop names

--- src/components/elements/element-group-note/current-note/CurrentNote.tsx
+++ src/components/elements/element-group-note/current-note/CurrentNote.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {ReactElement} from "react";
 import {Note} from "../../../../models/note/Note";
 import Title from "../../element-group-native/title/Title";
+import {TagType} from "../../../../models/html/TagType";
 
 interface CurrentNoteProps {
     currentNote: Note;
 }
 
-function CurrentNote(props: CurrentNoteProps) {
+function CurrentNote(props: CurrentNoteProps): ReactElement {
     return (
         <div className="current-note">
-            <Title title={props.currentNote.noteData.noteName} size={3} />
+            <Title text={props.currentNote.noteData.noteName} tagType={TagType.H3} />
         </div>
     );
 }
