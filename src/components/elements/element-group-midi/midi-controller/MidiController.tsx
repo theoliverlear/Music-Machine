396a735e74c450b1eaf32b63292d015f615316ae
theoliverlear <sigwartholiver2@gmail.com>
2025-06-07 19:47:13 -0500

    Refactor MidiController.tsx to add more explicit typings and remove commented-out code

--- src/components/elements/element-group-midi/midi-controller/MidiController.tsx
+++ src/components/elements/element-group-midi/midi-controller/MidiController.tsx
@@ -27,20 +27,15 @@ function MidiController(props: MidiControllerProps) {
                 let noteTimestamp: number = message.timeStamp;
                 let pressedNote: MidiNote = new MidiNote(noteId, velocity, noteTimestamp);
                 let note: Note = new Note(pressedNote);
-                setCurrentNotes(previousNotes => {
-                    const updatedNotes = [...previousNotes, note];
+                setCurrentNotes((previousNotes: Note[]): Note[] => {
+                    const updatedNotes: Note[] = [...previousNotes, note];
                     props.onNoteChange(updatedNotes);
-                    // console.log('Updated Notes: ', updatedNotes);
                     return updatedNotes;
                 });
-                // console.log('Note Pressed: ', note.toString());
-                // console.log('Current Notes: ', currentNotes);
             } else if (status === 128 || (status === 144 && velocity === 0)) {
-                // console.log('Note Released');
                 setCurrentNotes(previousNotes => {
                     const updatedNotes = previousNotes.filter((note: Note) => note.midiNote.noteId !== noteId)
                     props.onNoteChange(updatedNotes);
-                    // console.log('Updated Notes: ', updatedNotes);
                     return updatedNotes;
                 });
             }
@@ -51,7 +46,7 @@ function MidiController(props: MidiControllerProps) {
         }
     }, [midiInput, props]);
 return (
-    <div>
+    <div className={"midi-controller"}>
         <MidiPopup onMidiDeviceSelected={handleMidiDeviceSelection} />
         <MidiDevice input={midiInput} output={midiOutput} />
     </div>
