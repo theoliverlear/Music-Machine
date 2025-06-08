289a381f15d5cec5c45929e01ebe151ffbb5faa6
theoliverlear <sigwartholiver2@gmail.com>
2025-06-08 11:08:43 -0500

    Add PitchSlider.tsx to create a pitch notation selector with sharps or flats

---
+++ src/components/elements/element-group-setting/pitch-slider/PitchSlider.tsx
@@ -0,0 +1,14 @@
+// A slider that allows the user to adjust whether to use sharps or flats in the pitch notation.
+
+import React from 'react';
+import './PitchSlider.css';
+
+function PitchSlider() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default PitchSlider;
