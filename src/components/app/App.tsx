b0f7ecbeb739637e6e0edcb50f7657213f1fefd4
theoliverlear <sigwartholiver2@gmail.com>
2025-06-08 11:07:18 -0500

    Refactor App.tsx to remove unnecessary props

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,14 +3,13 @@ import './App.css';
 import FreePlay from "../pages/free-play/FreePlay";
 import Home from "../pages/home/Home";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
-const menuClassName: string = "menu-item";
 
 function App(): ReactElement {
   return (
       <Router>
           <Routes>
-              <Route path="/" element={<Home className={menuClassName}/>} />
-              <Route path="/free-play" element={<FreePlay/>} />
+              <Route path="/" element={<Home/>}/>
+              <Route path="/free-play" element={<FreePlay/>}/>
           </Routes>
       </Router>
   );
