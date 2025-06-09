c10dde920fa4bb6acca437bc0adec82aaf99861a
theoliverlear <sigwartholiver2@gmail.com>
2025-06-09 18:44:54 -0500

    Add utility functions for generating image asset paths in imageAssets.ts

--- src/assets/imageAssets.ts
+++ src/assets/imageAssets.ts
@@ -2,3 +2,22 @@ export interface ImageAsset {
     src: string;
     alt: string;
 }
+
+export function getImagePath(fileName: string): string {
+    return imageAssetPath + fileName;
+}
+export function getLogoImagePath(fileName: string): string {
+    return logoImageAssetPath + fileName;
+}
+export function getIconImagePath(fileName: string): string {
+    return iconImageAssetPath + fileName;
+}
+
+const imageAssetPath: string = 'images/';
+const logoImageAssetPath: string = imageAssetPath + 'logo/';
+const iconImageAssetPath: string = imageAssetPath + 'icon/';
+
+export const homeIconImageAsset: ImageAsset = {
+    src: getIconImagePath('home_icon.svg'),
+    alt: 'Home Icon'
+}
