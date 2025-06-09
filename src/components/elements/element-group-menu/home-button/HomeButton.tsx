129a07e99d29dbdef3e6d4c03e3339cbbdf99b1d
theoliverlear <sigwartholiver2@gmail.com>
2025-06-09 18:45:53 -0500

    Add HomeButton.tsx to implement home button with routing and icon display

---
+++ src/components/elements/element-group-menu/home-button/HomeButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "../../element-group-native/image/Image";
+import {homeIconImageAsset} from "../../../../assets/imageAssets";
+import './HomeButton.css';
+import {Link} from "react-router-dom";
+
+function HomeButton() {
+    return (
+        <Link to={"/"} className="link-no-style">
+            <div className={"home-button"}>
+                <Image imageAsset={homeIconImageAsset}/>
+            </div>
+        </Link>
+    )
+}
+export default HomeButton;
