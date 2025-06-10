8c8705c588e16ced4d8f3575be9e4db602c3e93c
theoliverlear <sigwartholiver2@gmail.com>
2025-06-10 16:27:03 -0500

    Add NavBar.tsx to implement navigation bar in the React app

---
+++ src/components/elements/element-group-menu/element-group-nav-bar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+import HomeButton from "../home-button/HomeButton";
+
+function NavBar() {
+    return (
+        <div className={"nav-bar"}>
+            <nav>
+                <HomeButton/>
+            </nav>
+        </div>
+    );
+}
+export default NavBar;
