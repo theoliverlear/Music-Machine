708b6ddefcc4744d3e0ef7000f704d7c147af6d3
theoliverlear <sigwartholiver2@gmail.com>
2025-06-10 22:02:26 -0500

    Expand Chord.test.ts to include tests for uninverted triads and input order independence

--- src/test/chord/Chord.test.ts
+++ src/test/chord/Chord.test.ts
@@ -3,7 +3,7 @@ import {Note} from "../../models/note/Note";
 import {MidiNote} from "../../models/midi/MidiNote";
 
 describe('Chord', (): void => {
-    it ('does not allow duplicate notes', (): void => {
+    it('does not allow duplicate notes', (): void => {
         let chord: Chord = new Chord();
         let noteBase: Note = new Note(new MidiNote(60));
         let noteDuplicate: Note = new Note(new MidiNote(60));
@@ -11,7 +11,29 @@ describe('Chord', (): void => {
         chord.addNote(noteDuplicate);
         expect(chord.notes.size).toBe(1);
     });
-    it ('calculates chord names', (): void => {
+    it('calculates without regard to input order', (): void => {
+        let chord: Chord = new Chord();
+        chord.addNote(new Note(new MidiNote(64))); // E
+        chord.addNote(new Note(new MidiNote(67))); // G
+        chord.addNote(new Note(new MidiNote(60))); // C
+        expect(chord.fullName).toBe("C Major");
+    });
 
+    it('identifies uninverted triads', () => {
+        let chord: Chord = new Chord();
+        chord.addNote(new Note(new MidiNote(60))); // C
+        chord.addNote(new Note(new MidiNote(64))); // E
+        chord.addNote(new Note(new MidiNote(67))); // G
+        expect(chord.isUninvertedTriad()).toBe(true);
+        chord = new Chord();
+        chord.addNote(new Note(new MidiNote(60))); // C
+        chord.addNote(new Note(new MidiNote(63))); // Eb
+        chord.addNote(new Note(new MidiNote(67))); // G
+        expect(chord.isUninvertedTriad()).toBe(true);
+        chord = new Chord();
+        chord.addNote(new Note(new MidiNote(60))); // C
+        chord.addNote(new Note(new MidiNote(65))); // F
+        chord.addNote(new Note(new MidiNote(67))); // G
+        expect(chord.isUninvertedTriad()).toBe(false);
     });
 });
