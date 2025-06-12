98baed873febf929f6326d0a40613294aa56d4a6
theoliverlear <sigwartholiver2@gmail.com>
2025-06-11 19:55:35 -0500

    Expand imageAssets.ts to include music theory icons for sharp and flat pitches

--- src/assets/imageAssets.ts
+++ src/assets/imageAssets.ts
@@ -13,11 +13,26 @@ export function getIconImagePath(fileName: string): string {
     return iconImageAssetPath + fileName;
 }
 
+export function getTheoryImagePath(fileName: string): string {
+    return theoryImageAssetPath + fileName;
+}
+
 const imageAssetPath: string = 'images/';
 const logoImageAssetPath: string = imageAssetPath + 'logo/';
 const iconImageAssetPath: string = imageAssetPath + 'icon/';
+const theoryImageAssetPath: string = imageAssetPath + 'theory/';
 
 export const homeIconImageAsset: ImageAsset = {
     src: getIconImagePath('home_icon.svg'),
     alt: 'Home Icon'
-}
+};
+
+export const sharpTheoryImageAsset: ImageAsset = {
+    src: getTheoryImagePath('sharp.svg'),
+    alt: 'Sharp Music Theory Icon'
+};
+
+export const flatTheoryImageAsset: ImageAsset = {
+    src: getTheoryImagePath('flat.svg'),
+    alt: 'Flat Music Theory Icon'
+};
