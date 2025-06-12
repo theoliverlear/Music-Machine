f2a96c6fb75b766b5000b0133b38da30fce49d03
theoliverlear <sigwartholiver2@gmail.com>
2025-06-11 20:02:02 -0500

    Refactor CurrentNote.tsx to display note text based on pitch type

--- src/components/elements/element-group-note/current-note/CurrentNote.tsx
+++ src/components/elements/element-group-note/current-note/CurrentNote.tsx
@@ -2,15 +2,29 @@ import React, {ReactElement} from "react";
 import {Note} from "../../../../models/note/Note";
 import Title from "../../element-group-native/title/Title";
 import {TagType} from "../../../../models/html/TagType";
+import {Pitch} from "../../element-group-setting/pitch-slider/models/types";
 
 interface CurrentNoteProps {
     currentNote: Note;
+    pitch?: Pitch;
 }
 
 function CurrentNote(props: CurrentNoteProps): ReactElement {
+    function getText(): string {
+        if (props.pitch) {
+            if (props.pitch === "flat") {
+                return props.currentNote.noteData.asFlat;
+            } else {
+                return props.currentNote.noteData.noteName;
+            }
+        } else {
+            return props.currentNote.noteData.asSharp;
+        }
+    }
+
     return (
         <div className="current-note">
-            <Title text={props.currentNote.noteData.noteName} tagType={TagType.H3} />
+            <Title text={getText()} tagType={TagType.H3} />
         </div>
     );
 }
