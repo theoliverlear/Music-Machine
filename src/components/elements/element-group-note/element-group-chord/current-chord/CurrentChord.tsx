59876086fb46710ae58ff4a6084a0cca052b19db
theoliverlear <sigwartholiver2@gmail.com>
2025-06-11 21:17:40 -0500

    Expand CurrentChord.tsx to display chord name based on pitch type and add pitch as an optional prop

--- src/components/elements/element-group-note/element-group-chord/current-chord/CurrentChord.tsx
+++ src/components/elements/element-group-note/element-group-chord/current-chord/CurrentChord.tsx
@@ -2,17 +2,28 @@ import React, {ReactElement} from 'react';
 import Title from "../../../element-group-native/title/Title";
 import {Chord} from "../../../../../models/chord/Chord";
 import {TagType} from "../../../../../models/html/TagType";
+import {
+    Pitch
+} from "../../../element-group-setting/pitch-slider/models/types";
 
 interface CurrentChordProps {
     currentChord: Chord;
+    pitch?: Pitch;
 }
 
 function CurrentChord(props: CurrentChordProps): ReactElement {
+    function getChordName(): string {
+        if (props.pitch) {
+            return props.currentChord.getPitchedFullName(props.pitch);
+        } else {
+            return props.currentChord.fullName;
+        }
+    }
     return (
         <div className={"current-chord"}>
             <Title text="Current Chord: " tagType={TagType.H4} />
-            {props.currentChord.notes.size == 0 && <Title text="None" tagType={TagType.H5} />}
-            {props.currentChord.notes.size != 0 && <Title text={props.currentChord.fullName} tagType={TagType.H5}/>}
+            {props.currentChord.notes.size === 0 && <Title text="None" tagType={TagType.H5} />}
+            {props.currentChord.notes.size !== 0 && <Title text={getChordName()} tagType={TagType.H5}/>}
         </div>
     );
 }
