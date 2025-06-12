cbea9b4fde7f43e2a51e04eeef4e34686ab49c3c
theoliverlear <sigwartholiver2@gmail.com>
2025-06-12 18:53:16 -0500

    Add PageTitle.tsx to render the page title component

---
+++ src/components/elements/element-group-text/page-title/PageTitle.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {TagType} from "../../../../models/html/TagType";
+import Title from "../../element-group-native/title/Title";
+import './PageTitle.css';
+
+interface PageTitleProps {
+    text: string;
+}
+
+function PageTitle(props: PageTitleProps) {
+    return (
+        <div className={"page-title"}>
+            <Title text={props.text} tagType={TagType.H1}/>
+        </div>
+    );
+}
+
+export default PageTitle;
