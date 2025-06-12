d9dad89fcd0a5e8bef3b0cd437e5bf7d596b34ec
theoliverlear <sigwartholiver2@gmail.com>
2025-06-11 21:27:22 -0500

    Expand ChordInterval.test.ts to test constructor behavior and validate interval additions

--- src/test/chord/ChordInterval.test.ts
+++ src/test/chord/ChordInterval.test.ts
@@ -4,11 +4,22 @@ import {Interval} from "../../models/chord/Interval";
 describe('ChordInterval', (): void => {
     describe('getNameByIntervals', (): void => {
         it('returns Major for major chord', (): void => {
-            let chordInterval: ChordInterval = new ChordInterval();
+            const chordInterval: ChordInterval = new ChordInterval();
             chordInterval.intervals.add(Interval.unison);
             chordInterval.intervals.add(Interval.majorThird);
             chordInterval.intervals.add(Interval.perfectFifth);
             expect(chordInterval.getNameByIntervals()).toBe('Major');
+        });
+    });
+    describe('constructor', (): void => {
+        it('adds the intervals passed', (): void => {
+            const intervals: Interval[] = [Interval.unison, Interval.majorThird, Interval.perfectFifth];
+            const chordInterval: ChordInterval = new ChordInterval(intervals);
+            expect(chordInterval.intervals.asArray.includes(Interval.unison)).toBe(true);
+            expect(chordInterval.intervals.asArray.includes(Interval.majorThird)).toBe(true);
+            expect(chordInterval.intervals.asArray.includes(Interval.perfectFifth)).toBe(true);
+            expect(chordInterval.intervals.asArray.length).toBe(3);
+            expect(chordInterval.intervals.asArray.includes(Interval.majorSixth)).toBe(false);
         });
     });
 });
