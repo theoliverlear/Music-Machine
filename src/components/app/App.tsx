3ea45a481f09d479b2058bd2bfb71e650e1482bb
theoliverlear <sigwartholiver2@gmail.com>
2025-06-11 19:57:41 -0500

    Refactor App.tsx to wrap `Router` with a container div for styling adaptability

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,14 +5,16 @@ import Home from "../pages/home/Home";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 
 function App(): ReactElement {
-  return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/free-play" element={<FreePlay/>}/>
-          </Routes>
-      </Router>
-  );
+    return (
+        <div className="app">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/free-play" element={<FreePlay/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
