07e9799590d26443cc98d53bafce51ad52184022
theoliverlear <sigwartholiver2@gmail.com>
2025-06-13 10:58:42 -0500

    Add SingleTrebleClef.tsx to create the SingleTrebleClef component in the React application

---
+++ src/components/elements/element-group-theory/element-group-clef/element-group-treble-clef/single-treble-clef/SingleTrebleClef.tsx
@@ -0,0 +1,12 @@
+import React, {ReactElement} from 'react';
+import './SingleTrebleClef.scss';
+
+function SingleTrebleClef(): ReactElement {
+    return (
+        <div className={"single-treble-clef"}>
+
+        </div>
+    );
+}
+
+export default SingleTrebleClef;
