a05ddc2f0186c231cc8df490065f605e0f66d01e
theoliverlear <sigwartholiver2@gmail.com>
2025-06-14 14:14:18 -0500

    Expand SheetMusic.tsx to integrate state hooks, pitch control, MIDI device handling, and live note rendering

--- src/components/pages/sheet-music/SheetMusic.tsx
+++ src/components/pages/sheet-music/SheetMusic.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SheetMusic.scss';
+import NavBar
+    from "../../elements/element-group-menu/element-group-nav-bar/nav-bar/NavBar";
+import PageTitle
+    from "../../elements/element-group-text/page-title/PageTitle";
+import MidiPiano
+    from "../../elements/element-group-midi/midi-piano/MidiPiano";
+import {Note} from "../../../models/note/Note";
+import LiveSheetNotes
+    from "../../elements/element-group-theory/element-group-note/live-sheet-notes/LiveSheetNotes";
+import PitchSlider
+    from "../../elements/element-group-setting/pitch-slider/PitchSlider";
+import {
+    Pitch
+} from "../../elements/element-group-setting/pitch-slider/models/types";
 
 function SheetMusic() {
+    const [currentNotes, setCurrentNotes] = useState<Note[]>([]);
+    const [midiDeviceSelected, setMidiDeviceSelected] = useState<boolean>(false);
+    const [pitchState, setPitchState] = useState<Pitch>("sharp");
 
+    function handlePitchChange(newPitch: Pitch): void {
+        setPitchState(newPitch);
+    }
+
+    function handleMidiDeviceSelection(isMidiDeviceSelected: boolean): void {
+        setMidiDeviceSelected(isMidiDeviceSelected);
+    }
+
+    function updateCurrentNotes(newNotes: Note[]): void {
+        if (JSON.stringify(currentNotes) === JSON.stringify(newNotes)) {
+            return;
+        }
+        setCurrentNotes(newNotes);
+    }
+
+    return (
+        <div className={"sheet-music"}>
+            <NavBar/>
+            <PitchSlider onPitchSelection={handlePitchChange}/>
+            <PageTitle text={"Sheet Music"}/>
+            <LiveSheetNotes currentNotes={currentNotes}/>
+            <MidiPiano onMidiDeviceSelected={handleMidiDeviceSelection}
+                       onNoteChange={updateCurrentNotes}/>
+        </div>
+    )
 }
 export default SheetMusic;
