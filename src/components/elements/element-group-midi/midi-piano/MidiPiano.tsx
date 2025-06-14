05211c7a192e0db7591db2eba1c11f9a463fa6eb
theoliverlear <sigwartholiver2@gmail.com>
2025-06-14 13:56:23 -0500

    Refactor MidiPiano.tsx to add state management, props, and integrate `MidiController` functionality

--- src/components/elements/element-group-midi/midi-piano/MidiPiano.tsx
+++ src/components/elements/element-group-midi/midi-piano/MidiPiano.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {ReactElement, useState} from 'react';
 import './MidiPiano.scss';
 import MidiPopup from "../midi-popup/MidiPopup";
 import Piano from "../../element-group-piano/piano/Piano";
+import {Note} from "../../../../models/note/Note";
+import MidiController from "../midi-controller/MidiController";
 
-function MidiPiano() {
+interface MidiPianoProps {
+    onNoteChange: (notes: Note[]) => void;
+    onMidiDeviceSelected: (isSelected: boolean) => void;
+}
+
+function MidiPiano(props: MidiPianoProps): ReactElement {
+    const [currentNotes, setCurrentNotes] = useState<Note[]>([]);
+    const [midiDeviceSelected, setMidiDeviceSelected] = useState<boolean>(false);
+    function handleMidiDeviceSelection(isMidiDeviceSelected: boolean): void {
+        setMidiDeviceSelected(isMidiDeviceSelected);
+        props.onMidiDeviceSelected(isMidiDeviceSelected);
+    }
+    function updateCurrentNotes(newNotes: Note[]): void {
+        setCurrentNotes(newNotes);
+        props.onNoteChange(newNotes);
+    }
+
     return (
         <div className={"midi-piano"}>
-            <MidiPopup onMidiDeviceSelected={() => null}/>
-            <Piano currentNotes={[]}/>
+            <MidiController onMidiDeviceSelected={handleMidiDeviceSelection}
+                            onNoteChange={updateCurrentNotes}/>
+            <Piano currentNotes={currentNotes}/>
         </div>
     );
 }
