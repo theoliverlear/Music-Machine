ea058e64c3c6520cb7967d6968a4508d9e07780a
theoliverlear <sigwartholiver2@gmail.com>
2025-06-14 20:53:42 -0500

    Expand CurrentNotes.tsx to include pitch type handling and dynamic pitch adjustments

--- src/components/elements/element-group-note/current-notes/CurrentNotes.tsx
+++ src/components/elements/element-group-note/current-notes/CurrentNotes.tsx
@@ -4,23 +4,63 @@ import CurrentNote from "../current-note/CurrentNote";
 import Title from "../../element-group-native/title/Title";
 import './CurrentNotes.scss';
 import {TagType} from "../../../../models/html/TagType";
-import {Pitch} from "../../element-group-setting/pitch-slider/models/types";
+import {
+    Pitch,
+    PitchType
+} from "../../element-group-setting/pitch-slider/models/types";
+import {NoteFactory} from "../../../../models/note/NoteFactory";
 
 interface CurrentNotesProps {
     currentNotes: Note[];
     pitch?: Pitch;
+    pitchType?: PitchType;
 }
 
 function CurrentNotes(props: CurrentNotesProps): ReactElement {
     function getCurrentNotes(): Note[] {
-        return props.currentNotes.sort((noteOne, noteTwo) => noteOne.noteNumber - noteTwo.noteNumber);
+        if (props.pitch && props.pitchType) {
+            if (props.pitchType === "auto") {
+                return NoteFactory.getNoteWithPitches(props.currentNotes)[0];
+            } else if (props.pitchType === "manual") {
+                return props.currentNotes;
+            }
+        }
+        return props.currentNotes;
     }
+    function getCurrentPitches(): Pitch[] {
+        let noteWithPitches: Pitch[] = NoteFactory.getNoteWithPitches(props.currentNotes)[1];
+        if (props.pitchType === "auto") {
+            return noteWithPitches;
+        }
+        if (props.pitch) {
+            for (let i = 0; i < noteWithPitches.length; i++) {
+                const currentPitch: Pitch = noteWithPitches[i];
+                if (currentPitch === "sharp") {
+                    if (props.pitch === "flat") {
+                        noteWithPitches[i] = "flat";
+                    }
+                } else if (currentPitch === "flat") {
+                    if (props.pitch === "sharp") {
+                        noteWithPitches[i] = "sharp";
+                    }
+                }
+            }
+        }
+        return noteWithPitches;
+    }
+
     return (
         <div className={"current-notes"}>
             <Title text="Current Notes: " tagType={TagType.H4} />
             {getCurrentNotes().length === 0 && <Title text={"None"} tagType={TagType.H5}/>}
             <div className={"notes"}>
-                {getCurrentNotes().map((note, index) => <CurrentNote key={index} currentNote={note} pitch={props.pitch}/>)}
+                {getCurrentNotes().map((note: Note, index: number) => (
+                    <CurrentNote
+                        key={index}
+                        currentNote={note}
+                        pitch={getCurrentPitches()[index]}
+                    />
+                ))}
             </div>
         </div>
     );
