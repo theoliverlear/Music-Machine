0e12c20ab939cd12c38a30897dc32a938a6c29fc
theoliverlear <sigwartholiver2@gmail.com>
2025-06-14 20:57:31 -0500

    Expand FreePlay.tsx to integrate `PitchType` handling, auto-pitch logic, and dynamic pitch adjustments

--- src/components/pages/free-play/FreePlay.tsx
+++ src/components/pages/free-play/FreePlay.tsx
@@ -16,14 +16,16 @@ import NavBar
 import PitchSlider
     from "../../elements/element-group-setting/pitch-slider/PitchSlider";
 import {
-    Pitch
+    Pitch, PitchType
 } from "../../elements/element-group-setting/pitch-slider/models/types";
 import PageTitle
     from "../../elements/element-group-text/page-title/PageTitle";
+import {ChordFactory} from "../../../models/chord/ChordFactory";
 
 
 function FreePlay() {
-    const [pitchState, setPitchState] = useState<Pitch>("sharp");
+    const [pitchState, setPitchState] = useState<Pitch>("natural");
+    const [pitchType, setPitchType] = useState<PitchType>("auto");
     const [currentNotes, setCurrentNotes] = useState<Note[]>([]);
     const [currentChord, setCurrentChord] = useState<Chord>(new Chord());
     const [midiDeviceSelected, setMidiDeviceSelected] = useState<boolean>(false);
@@ -40,20 +42,34 @@ function FreePlay() {
     function handleMidiDeviceSelection(isMidiDeviceSelected: boolean): void {
         setMidiDeviceSelected(isMidiDeviceSelected);
     }
+
+    function handlePitchTypeSelection(newPitchType: PitchType): void {
+        setPitchType(newPitchType);
+    }
+
+    function getPitch() {
+        if (pitchType === "auto" && pitchState === "natural") {
+            return ChordFactory.getAutoPitch(currentNotes);
+        } else {
+            return pitchState;
+        }
+    }
+
     return (
         <div className={"free-play"}>
             <NavBar/>
-            <PitchSlider onPitchSelection={handlePitchChange}/>
+            <PitchSlider onPitchSelection={handlePitchChange}
+            onPitchTypeSelection={handlePitchTypeSelection}/>
             <PageTitle text="Free Play"/>
             <MidiController
                 onMidiDeviceSelected={handleMidiDeviceSelection}
                 onNoteChange={updateCurrentNotes}/>
 
             {midiDeviceSelected &&
-                <CurrentChord currentChord={currentChord} pitch={pitchState}/>}
+                <CurrentChord currentChord={currentChord} pitch={getPitch()}/>}
 
             {midiDeviceSelected &&
-                <CurrentNotes currentNotes={currentNotes} pitch={pitchState}/>}
+                <CurrentNotes currentNotes={currentNotes} pitch={getPitch()}/>}
             <Piano currentNotes={currentNotes}/>
         </div>
     );
