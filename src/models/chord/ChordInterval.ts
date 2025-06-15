3127b4ae2fdb3148f2d0079a8d16cd991dd53fc5
theoliverlear <sigwartholiver2@gmail.com>
2025-06-15 14:57:12 -0500

    Refactor ChordInterval.ts to add comments for structure and reorganize methods

--- src/models/chord/ChordInterval.ts
+++ src/models/chord/ChordInterval.ts
@@ -2,9 +2,10 @@ import {Interval} from "./Interval";
 import {MusicSet} from "../MusicSet";
 
 export class ChordInterval {
+    //============================-Variables-=================================
     private _intervals: MusicSet<Interval>;
     private _name: string;
-
+    //===========================-Constructor-================================
     constructor(intervals: Interval[] = [], name: string = '') {
         this._intervals = new MusicSet<Interval>(intervals);
         if (name === '') {
@@ -13,6 +14,9 @@ export class ChordInterval {
             this._name = name;
         }
     }
+    //=============================-Methods-==================================
+
+    //--------------------------Equals-Intervals------------------------------
     equalIntervals(chordInterval: Interval[]): boolean {
         if (this._intervals.asArray().length !== chordInterval.length) {
             return false;
@@ -27,13 +31,13 @@ export class ChordInterval {
         }
         return true;
     }
-
+    //----------------------Sort-Smallest-To-Largest--------------------------
     sortSmallestToLargest(): void {
         this._intervals.asArray().sort((intervalOne: Interval, intervalTwo: Interval): number => {
             return intervalOne.semitones - intervalTwo.semitones;
         });
     }
-
+    //-----------------------Get-Name-By-Intervals----------------------------
     getNameByIntervals(): string {
         let chordName: string = 'Unknown';
         for (const chordInterval of ChordInterval.intervals) {
@@ -44,20 +48,23 @@ export class ChordInterval {
         }
         return chordName;
     }
-    get intervals(): MusicSet<Interval> {
-        // this.sortSmallestToLargest();
-        return this._intervals;
-    }
-    set intervals(intervals: MusicSet<Interval>) {
-        this._intervals = intervals;
-    }
+    //-------------------------------Equals-----------------------------------
     equals(chordInterval: ChordInterval): boolean {
         return this._intervals.asArray().every((interval: Interval, index: number): boolean => {
             return interval === chordInterval.intervals.asArray()[index];
         });
     }
+    //=============================-Getters-==================================
+    get intervals(): MusicSet<Interval> {
+        return this._intervals;
+    }
+    //=============================-Setters-==================================
+    set intervals(intervals: MusicSet<Interval>) {
+        this._intervals = intervals;
+    }
+    //============================-Constants-=================================
 
-    // Base
+    //---------------------------Base-Intervals-------------------------------
     static majorIntervals: Interval[] =
         [Interval.unison, Interval.majorThird, Interval.perfectFifth];
 
@@ -84,7 +91,8 @@ export class ChordInterval {
 
     static diminishedSeventhIntervals: Interval[] =
         [Interval.unison, Interval.minorThird, Interval.tritone, Interval.minorSeventh];
-    // Triad Inversions
+
+    //---------------------Triad-Inversions-Intervals-------------------------
     static majorFirstInversionIntervals: Interval[] =
         [Interval.unison, Interval.minorThird, Interval.minorSixth];
 
@@ -108,54 +116,54 @@ export class ChordInterval {
 
     static augmentedSecondInversionIntervals: Interval[] =
         [Interval.unison, Interval.majorThird, Interval.minorSixth];
-    // 7th Inversions
+
+    //----------------------7th-Inversions-Intervals--------------------------
     static majorSeventhFirstInversionIntervals: Interval[] =
-        [Interval.unison, Interval.minorThird, Interval.perfectFifth, Interval.minorSixth];  // 0-3-7-8  (6-5)
+        [Interval.unison, Interval.minorThird, Interval.perfectFifth, Interval.minorSixth];
 
     static majorSeventhSecondInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorThird, Interval.perfectFourth, Interval.majorSixth]; // 0-4-5-9  (4-3)
+        [Interval.unison, Interval.majorThird, Interval.perfectFourth, Interval.majorSixth];
 
     static majorSeventhThirdInversionIntervals:  Interval[] =
-        [Interval.unison, Interval.minorSecond, Interval.perfectFourth, Interval.minorSixth]; // 0-1-5-8  (4-2)
+        [Interval.unison, Interval.minorSecond, Interval.perfectFourth, Interval.minorSixth];
 
     static dominantFirstInversionIntervals: Interval[] =
-        [Interval.unison, Interval.minorThird,  Interval.tritone, Interval.minorSixth];  // 0-3-6-8  (6-5)
+        [Interval.unison, Interval.minorThird,  Interval.tritone, Interval.minorSixth];
 
     static dominantSecondInversionIntervals: Interval[] =
-        [Interval.unison, Interval.minorThird, Interval.perfectFourth, Interval.majorSixth]; // 0-3-5-9  (4-3)
+        [Interval.unison, Interval.minorThird, Interval.perfectFourth, Interval.majorSixth];
 
     static dominantThirdInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorSecond, Interval.tritone, Interval.majorSixth]; // 0-2-6-9  (4-2)
+        [Interval.unison, Interval.majorSecond, Interval.tritone, Interval.majorSixth];
 
     static minorSeventhFirstInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorThird, Interval.perfectFifth, Interval.majorSixth]; // 0-4-7-9
+        [Interval.unison, Interval.majorThird, Interval.perfectFifth, Interval.majorSixth];
 
     static minorSeventhSecondInversionIntervals: Interval[] =
-        [Interval.unison, Interval.minorThird, Interval.perfectFourth, Interval.minorSixth]; // 0-3-5-8
+        [Interval.unison, Interval.minorThird, Interval.perfectFourth, Interval.minorSixth];
 
     static minorSeventhThirdInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorSecond, Interval.perfectFourth, Interval.majorSixth]; // 0-2-5-9
+        [Interval.unison, Interval.majorSecond, Interval.perfectFourth, Interval.majorSixth];
 
     static minorMajorSeventhFirstInversionIntervals:  Interval[] =
-        [Interval.unison, Interval.majorThird, Interval.minorSixth, Interval.majorSixth]; // 0-4-8-9
+        [Interval.unison, Interval.majorThird, Interval.minorSixth, Interval.majorSixth];
 
     static minorMajorSeventhSecondInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorThird, Interval.perfectFourth, Interval.minorSixth]; // 0-4-5-8
+        [Interval.unison, Interval.majorThird, Interval.perfectFourth, Interval.minorSixth];
 
     static minorMajorSeventhThirdInversionIntervals: Interval[] =
-        [Interval.unison, Interval.minorSecond, Interval.minorThird, Interval.minorSixth]; // 0-1-3-8
+        [Interval.unison, Interval.minorSecond, Interval.minorThird, Interval.minorSixth];
 
     static diminishedSeventhFirstInversionIntervals:  Interval[] =
-        [Interval.unison, Interval.minorThird, Interval.perfectFifth, Interval.majorSixth]; // 0-3-7-9
+        [Interval.unison, Interval.minorThird, Interval.perfectFifth, Interval.majorSixth];
 
     static diminishedSeventhSecondInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorThird, Interval.perfectFourth, Interval.minorSixth]; // 0-4-5-8
+        [Interval.unison, Interval.majorThird, Interval.perfectFourth, Interval.minorSixth];
 
     static diminishedSeventhThirdInversionIntervals: Interval[] =
-        [Interval.unison, Interval.majorSecond, Interval.perfectFourth, Interval.minorSixth]; // 0-2-5-8
+        [Interval.unison, Interval.majorSecond, Interval.perfectFourth, Interval.minorSixth];
 
-
-    // Base
+    //------------------------Base-Chord-Intervals----------------------------
     static major: ChordInterval = new ChordInterval(
         ChordInterval.majorIntervals, 'Major');
 
@@ -183,7 +191,7 @@ export class ChordInterval {
     static diminishedSeventh: ChordInterval = new ChordInterval(
         ChordInterval.diminishedSeventhIntervals, 'Diminished Seventh');
 
-    // Triad Inversions
+    //-----------------------Triad-Chord-Intervals----------------------------
     static majorFirstInversion: ChordInterval = new ChordInterval(
         ChordInterval.majorFirstInversionIntervals, 'Major (1st Inversion)');
 
@@ -202,14 +210,13 @@ export class ChordInterval {
     static diminishedSecondInversion: ChordInterval = new ChordInterval(
         ChordInterval.diminishedSecondInversionIntervals, 'Diminished (2nd Inversion)');
 
-
     static augmentedFirstInversion: ChordInterval = new ChordInterval(
         ChordInterval.augmentedFirstInversionIntervals, 'Augmented (1st Inversion)');
 
     static augmentedSecondInversion: ChordInterval = new ChordInterval(
         ChordInterval.augmentedSecondInversionIntervals, 'Augmented (2nd Inversion)');
 
-    // 7th Inversions
+    //------------------------7th-Chord-Intervals-----------------------------
     static majorSeventhFirstInversion: ChordInterval = new ChordInterval(
         ChordInterval.majorSeventhFirstInversionIntervals, 'Major Seventh (1st Inversion)');
 
@@ -218,7 +225,6 @@ export class ChordInterval {
 
     static majorSeventhThirdInversion: ChordInterval = new ChordInterval(
         ChordInterval.majorSeventhThirdInversionIntervals, 'Major Seventh (3rd Inversion)');
-
 
     static dominantFirstInversion: ChordInterval = new ChordInterval(
         ChordInterval.dominantFirstInversionIntervals, 'Dominant (1st Inversion)');
@@ -240,18 +246,22 @@ export class ChordInterval {
 
     static minorMajorSeventhFirstInversion: ChordInterval = new ChordInterval(
         ChordInterval.minorMajorSeventhFirstInversionIntervals, 'Minor Major 7 (1st Inversion)');
+
     static minorMajorSeventhSecondInversion: ChordInterval = new ChordInterval(
         ChordInterval.minorMajorSeventhSecondInversionIntervals, 'Minor Major 7 (2nd Inversion)');
+
     static minorMajorSeventhThirdInversion: ChordInterval = new ChordInterval(
         ChordInterval.minorMajorSeventhThirdInversionIntervals, 'Minor Major 7 (3rd Inversion)');
 
     static diminishedSeventhFirstInversion: ChordInterval = new ChordInterval(
         ChordInterval.diminishedSeventhFirstInversionIntervals, 'Diminished Seventh (1st Inversion)');
+
     static diminishedSeventhSecondInversion: ChordInterval = new ChordInterval(
         ChordInterval.diminishedSeventhSecondInversionIntervals, 'Diminished Seventh (2nd Inversion)');
+
     static diminishedSeventhThirdInversion: ChordInterval = new ChordInterval(
         ChordInterval.diminishedSeventhThirdInversionIntervals, 'Diminished Seventh (3rd Inversion)');
-
+    //-----------------------------Intervals----------------------------------
     static intervals: ChordInterval[] = [
         ChordInterval.major,
         ChordInterval.minor,
