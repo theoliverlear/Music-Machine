a9432e98d374ec4435e36495ce6dba4cca3662a6
theoliverlear <sigwartholiver2@gmail.com>
2025-06-16 18:59:52 -0500

    Add `AnalogousKeys` type to represent a major and relative minor key pairing in types.ts

--- src/models/signature/types.ts
+++ src/models/signature/types.ts
@@ -1,1 +1,8 @@
+import {KeySignature} from "./KeySignature";
+
 export type PitchAccidental = "none" | "sharp" | "flat" | "natural";
+
+export type AnalogousKeys = {
+    major: KeySignature,
+    minor: KeySignature
+};
