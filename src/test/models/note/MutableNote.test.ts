aa11843cd8a3d6e59e3f2d3fa0dd3db50136715b
theoliverlear <sigwartholiver2@gmail.com>
2025-06-17 09:35:26 -0500

    Add tests for `mutateToKeySignature()` in MutableNote.test.ts to verify mutation behavior from key signatures

--- src/test/models/note/MutableNote.test.ts
+++ src/test/models/note/MutableNote.test.ts
@@ -1,8 +1,49 @@
 import { MidiNote } from "../../../models/midi/MidiNote";
 import {Note} from "../../../models/note/Note";
 import {MutableNote} from "../../../models/note/MutableNote";
+import {KeySignature} from "../../../models/signature/KeySignature";
 
 describe('MutableNote', (): void => {
+    describe('mutateToKeySignature', (): void => {
+        it('can mutate to a natural key signature', (): void => {
+            const keySignature: KeySignature = KeySignature.cMajor;
+            const noteOne: MutableNote = MutableNote.fromMidiNumber(60); // C
+            noteOne.mutateToKeySignature(keySignature);
+            expect(noteOne.mutatedName).toBe("C");
+            const noteTwo: MutableNote = MutableNote.fromMidiNumber(63); // D#
+            noteTwo.mutateToKeySignature(keySignature);
+            expect(noteTwo.mutatedName).toBe("D#");
+            const noteThree: MutableNote = MutableNote.fromMidiNumber(66); // F# || Gb
+            noteThree.mutate("flat");
+            noteThree.mutateToKeySignature(keySignature);
+            expect(noteThree.mutatedName).toBe("Gb");
+        });
+        it('can mutate to a key signature with sharps', (): void => {
+            const keySignature: KeySignature = KeySignature.fSharpMinor;
+            const noteOne: MutableNote = MutableNote.fromMidiNumber(60); // C
+            noteOne.mutateToKeySignature(keySignature);
+            expect(noteOne.mutatedName).toBe("C");
+            const noteTwo: MutableNote = MutableNote.fromMidiNumber(68); // G# || Ab
+            noteTwo.mutate("flat");
+            noteTwo.mutateToKeySignature(keySignature);
+            expect(noteTwo.mutatedName).toBe("G#");
+            const noteThree: MutableNote = MutableNote.fromMidiNumber(61); // C#
+            noteThree.mutateToKeySignature(keySignature);
+            expect(noteThree.mutatedName).toBe("C#");
+        });
+        it('can mutate to a key signature with flats', (): void => {
+            const keySignature: KeySignature = KeySignature.bFlatMajor;
+            const noteOne: MutableNote = MutableNote.fromMidiNumber(60); // C
+            noteOne.mutateToKeySignature(keySignature);
+            expect(noteOne.mutatedName).toBe("C");
+            const noteTwo: MutableNote = MutableNote.fromMidiNumber(63); // D# || Eb
+            noteTwo.mutateToKeySignature(keySignature);
+            expect(noteTwo.mutatedName).toBe("Eb");
+            const noteThree: MutableNote = MutableNote.fromMidiNumber(66); // F# || Gb
+            noteThree.mutateToKeySignature(keySignature);
+            expect(noteThree.mutatedName).toBe("Gb");
+        });
+    });
     describe('mutate', (): void => {
         it('should not mutate already mutated notes', (): void => {
             const noteOne: Note = new Note(new MidiNote(61)); // C#
