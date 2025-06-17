07c408c75a07b374d9970814dba658f158c05139
theoliverlear <sigwartholiver2@gmail.com>
2025-06-17 09:31:32 -0500

    Refactor `PitchAccidental` type in types.ts to be a union of "none" and `Pitch` type for modularity

--- src/models/signature/types.ts
+++ src/models/signature/types.ts
@@ -1,6 +1,9 @@
 import {KeySignature} from "./KeySignature";
+import {
+    Pitch
+} from "../../components/elements/element-group-setting/pitch-slider/models/types";
 
-export type PitchAccidental = "none" | "sharp" | "flat" | "natural";
+export type PitchAccidental = "none" | Pitch;
 
 export type AnalogousKeys = {
     major: KeySignature,
