41c36abd357e4083fc64f06fd975556acb68c2e5
theoliverlear <sigwartholiver2@gmail.com>
2025-06-18 11:29:33 -0500

    Add `ChordCallout` route to Home.tsx

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -13,7 +13,19 @@ function Home(): ReactElement {
             <hr/>
             <div className={"menu-item"}>
                 <Link to={"/free-play"} className="link-no-style">
-                    <Title text="Free Play" tagType={TagType.H3} />
+                    <Title text="Free Play" tagType={TagType.H3}/>
+                </Link>
+            </div>
+
+            <div className={"menu-item"}>
+                <Link to={"/sheet-music"} className="link-no-style">
+                    <Title text="Sheet Music" tagType={TagType.H3}/>
+                </Link>
+            </div>
+
+            <div className={"menu-item"}>
+                <Link to={"/chord-callout"} className="link-no-style">
+                    <Title text="Chord Callout" tagType={TagType.H3}/>
                 </Link>
             </div>
 
@@ -23,21 +35,16 @@ function Home(): ReactElement {
                 </Link>
             </div>
 
-            <div className={"menu-item"}>
-                <Link to={"/sheet-music"} className="link-no-style">
-                    <Title text="Sheet Music" tagType={TagType.H3} />
-                </Link>
-            </div>
 
-            <div className={"menu-item"}>
-                <Link to={"/chord-builder"} className="link-no-style">
-                    <Title text="Chord Builder" tagType={TagType.H3} />
-                </Link>
-            </div>
+            {/*<div className={"menu-item"}>*/}
+            {/*    <Link to={"/chord-builder"} className="link-no-style">*/}
+            {/*        <Title text="Chord Builder" tagType={TagType.H3} />*/}
+            {/*    </Link>*/}
+            {/*</div>*/}
 
-            <div className={"menu-item"}>
-                <Title text="Learn Songs" tagType={TagType.H3} />
-            </div>
+            {/*<div className={"menu-item"}>*/}
+            {/*    <Title text="Learn Songs" tagType={TagType.H3} />*/}
+            {/*</div>*/}
         </div>
     );
 }
