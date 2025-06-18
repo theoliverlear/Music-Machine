58aba416ed9e861f9416ceb87f1ed09240d00646
theoliverlear <sigwartholiver2@gmail.com>
2025-06-18 11:32:26 -0500

    Add ChordDifficulty.ts to define chord difficulty levels

---
+++ src/models/chord/ChordDifficulty.ts
@@ -0,0 +1,24 @@
+export enum ChordDifficulty {
+    EASY = 3,
+    MEDIUM = 4,
+    HARD = 5,
+    EXPERT = 6,
+}
+export namespace ChordDifficulty {
+    export function getDifficulty(numNotes: number,
+                                  numInversions: number): ChordDifficulty {
+        if (numNotes < 3 || numInversions < 0) {
+            throw new Error("Invalid number of notes or inversions for " +
+                            "chord difficulty calculation.");
+        }
+        if (numNotes > 6 || numInversions > 3) {
+            throw new Error("Number of notes or inversions exceeds the " +
+                            "maximum for chord difficulty calculation.");
+        }
+        const totalDifficulty: number = numNotes + numInversions;
+        if (totalDifficulty > ChordDifficulty.EXPERT) {
+            return ChordDifficulty.EXPERT;
+        }
+        return totalDifficulty as ChordDifficulty;
+    }
+}
