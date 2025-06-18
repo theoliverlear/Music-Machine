49520c29bd585e753e71f3ce46c59a1b10c0752d
theoliverlear <sigwartholiver2@gmail.com>
2025-06-18 11:26:28 -0500

    Add ChordCalloutPicker.tsx to implement a mode picker for the "Chord Callout" game

---
+++ src/components/elements/element-group-menu/element-group-chord-callout/element-group-chord-callout-picker/chord-callout-picker/ChordCalloutPicker.tsx
@@ -0,0 +1,10 @@
+import React, {ReactElement} from 'react';
+import './ChordCalloutPicker.scss';
+
+function ChordCalloutPicker(): ReactElement {
+    return (
+        <div className={"chord-callout-picker"}>
+
+        </div>
+    )
+}
