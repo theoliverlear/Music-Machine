d897cb37887ca191543ec381b01eb06dbee1e5de
theoliverlear <sigwartholiver2@gmail.com>
2025-06-19 17:17:55 -0500

    Expand Piano.tsx to add octave limiting functionality and refactor repetitive octave rendering logic

--- src/components/elements/element-group-piano/piano/Piano.tsx
+++ src/components/elements/element-group-piano/piano/Piano.tsx
@@ -4,28 +4,72 @@ import KeyOctave from "../element-group-octave/key-octave/KeyOctave";
 import ClosingOctave from "../element-group-octave/element-group-closing/closing-octave/ClosingOctave";
 import './Piano.scss';
 import {Note} from "../../../../models/note/Note";
+import {NoteData} from "../../../../models/note/NoteData";
 
 interface PianoProps {
     currentNotes: Note[];
+    keyLimits?: [number, number];
 }
 function Piano(props: PianoProps): ReactElement {
     function isNotePlayed(dataNote: string): boolean {
         const [firstNote, secondNote] = dataNote.split(" | ");
-        let isPlayed: boolean = props.currentNotes.some(note => note.noteData.fullNoteName === firstNote || note.noteData.fullNoteName === secondNote);
+        const isPlayed: boolean = props.currentNotes.some(note => note.noteData.fullNoteName === firstNote || note.noteData.fullNoteName === secondNote);
         return isPlayed;
     }
+    function limitOctave(limits: [number, number] | undefined, octave: number): boolean {
+        if (!limits) {
+            return false;
+        }
+        const lowNote: NoteData = NoteData.getByNoteNumber(limits[0]);
+        const highNote: NoteData = NoteData.getByNoteNumber(limits[1]);
+        return lowNote.octave > octave || highNote.octave < octave;
+    }
+
+    function getElementByOctave(octaveNumber: number) {
+        switch (octaveNumber) {
+            case 0:
+                return (
+                    <>
+                        {!limitOctave(props.keyLimits, 0) && (
+                            <OpeningOctave currentNotes={props.currentNotes}
+                                           isNotePlayed={isNotePlayed}/>
+                        )}
+                    </>
+                );
+            case 8:
+                return (
+                    <>
+                        {!limitOctave(props.keyLimits, 8) && (
+                            <ClosingOctave currentNotes={props.currentNotes}
+                                           isNotePlayed={isNotePlayed}/>
+                        )}
+                    </>
+                );
+            default:
+                return (
+                    <>
+                        {!limitOctave(props.keyLimits, octaveNumber) && (
+                            <KeyOctave octaveNumber={octaveNumber}
+                                       currentNotes={props.currentNotes}
+                                       isNotePlayed={isNotePlayed}/>
+                        )}
+                    </>
+                )
+        }
+    }
+
     return (
         <div className="piano">
             <div className="key-container">
-                <OpeningOctave currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={1}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={2}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={3}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={4}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={5}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={6}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <KeyOctave octaveNumber={7}  currentNotes={props.currentNotes} isNotePlayed={isNotePlayed}/>
-                <ClosingOctave currentNotes={props.currentNotes} isNotePlayed={isNotePlayed} />
+                {getElementByOctave(0)}
+                {getElementByOctave(1)}
+                {getElementByOctave(2)}
+                {getElementByOctave(3)}
+                {getElementByOctave(4)}
+                {getElementByOctave(5)}
+                {getElementByOctave(6)}
+                {getElementByOctave(7)}
+                {getElementByOctave(8)}
             </div>
         </div>
     );
