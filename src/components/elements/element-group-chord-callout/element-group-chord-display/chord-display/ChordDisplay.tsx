ff1e89b1aedb603a3e48cc813e6304d4205052f6
theoliverlear <sigwartholiver2@gmail.com>
2025-06-21 15:53:42 -0500

    Expand ChordDisplay.tsx to introduce typed props and dynamic rendering based on chord type

--- src/components/elements/element-group-chord-callout/element-group-chord-display/chord-display/ChordDisplay.tsx
+++ src/components/elements/element-group-chord-callout/element-group-chord-display/chord-display/ChordDisplay.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import './ChordDisplay.scss';
+import {
+    ChordCalloutOptionType
+} from "../../element-group-chord-callout-picker/chord-callout-option/models/ChordCalloutOptionType";
+import ChordKeyDisplay from "../chord-key-display/ChordKeyDisplay";
+import ChordSheetDisplay from "../chord-sheet-display/ChordSheetDisplay";
+import ChordNameDisplay from "../chord-name-display/ChordNameDisplay";
+import {Note} from "../../../../../models/note/Note";
 
-function ChordDisplay() {
+interface ChordDisplayProps {
+    chordType: ChordCalloutOptionType;
+    currentNotes: Note[];
+}
 
+function ChordDisplay(props: ChordDisplayProps): ReactElement {
+    function getDisplayElement(): ReactElement {
+        switch (props.chordType) {
+            case ChordCalloutOptionType.KEYS:
+                return <ChordKeyDisplay currentNotes={props.currentNotes}/>;
+            case ChordCalloutOptionType.SHEET_MUSIC:
+                return <ChordSheetDisplay/>;
+            case ChordCalloutOptionType.NAME:
+                return <ChordNameDisplay/>;
+            default:
+                throw new Error("Unknown chord type: " + props.chordType);
+        }
+    }
+
+    return (
+        <div className={"chord-display"}>
+            {getDisplayElement()}
+        </div>
+    );
 }
 
 export default ChordDisplay;
