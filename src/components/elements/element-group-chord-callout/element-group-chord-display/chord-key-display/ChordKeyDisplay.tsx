28e3bdef733a7fecd16ba83eb98b12ef0697a03c
theoliverlear <sigwartholiver2@gmail.com>
2025-06-21 15:55:35 -0500

    Add ChordKeyDisplay.tsx to implement a component for rendering a piano chord

---
+++ src/components/elements/element-group-chord-callout/element-group-chord-display/chord-key-display/ChordKeyDisplay.tsx
@@ -0,0 +1,24 @@
+import React, {ReactElement} from 'react';
+import './ChordKeyDisplay.scss';
+import {Note} from "../../../../../models/note/Note";
+import Piano from "../../../element-group-piano/piano/Piano";
+
+interface ChordDisplayProps {
+    currentNotes: Note[];
+}
+
+function ChordKeyDisplay(props: ChordDisplayProps): ReactElement {
+    function getKeyLimits(): [number, number] {
+        const firstNote: Note = props.currentNotes[0];
+        const lastNote: Note = props.currentNotes[props.currentNotes.length - 1];
+        return [firstNote.noteData.noteNumber, lastNote.noteData.noteNumber];
+    }
+
+    return (
+        <div className={"chord-key-display"}>
+            <Piano currentNotes={props.currentNotes} keyLimits={getKeyLimits()}/>
+        </div>
+    );
+}
+
+export default ChordKeyDisplay;
