67f95807c895d0accbd6bcbdb0bac1407311221f
theoliverlear <sigwartholiver2@gmail.com>
2025-06-22 14:58:23 -0500

    Expand ChordCalloutGame.ts to include random chord generation and difficulty-based logic

--- src/components/pages/chord-callout/models/ChordCalloutGame.ts
+++ src/components/pages/chord-callout/models/ChordCalloutGame.ts
@@ -1,4 +1,8 @@
 import {Chord} from "../../../../models/chord/Chord";
+import {ChordDifficulty} from "../../../../models/chord/ChordDifficulty";
+import {ChordInterval} from "../../../../models/chord/ChordInterval";
+import {Interval} from "../../../../models/chord/Interval";
+import {Note} from "../../../../models/note/Note";
 
 export class ChordCalloutGame {
     private _score: number;
@@ -13,6 +17,52 @@ export class ChordCalloutGame {
         this._currentChordIndex = 0;
     }
 
+    public generateRandomChord(difficulty: ChordDifficulty): Chord {
+        switch (difficulty) {
+            case ChordDifficulty.EASY:
+                return this.getEasyChord();
+            default:
+                throw new Error(`Unsupported chord difficulty: ${difficulty}`);
+        }
+    }
+
+
+    private getNoteInversionSplit(difficulty: ChordDifficulty): [number, number] {
+        let numNotes: number;
+        let inversionSplit: number;
+        switch (difficulty) {
+            case ChordDifficulty.EASY:
+                throw new Error("Inversion split not implemented for easy difficulty");
+            case ChordDifficulty.MEDIUM:
+                // A random number between 0 and 1 (only as integers)
+                inversionSplit = Math.floor(Math.random() * 2);
+                numNotes = difficulty - inversionSplit;
+                return [numNotes, inversionSplit];
+            case ChordDifficulty.HARD:
+                inversionSplit = Math.floor(Math.random() * 2);
+                numNotes = difficulty - inversionSplit;
+                return [numNotes, inversionSplit];
+            case ChordDifficulty.EXPERT:
+                inversionSplit = Math.floor(Math.random() * 3);
+                numNotes = difficulty - inversionSplit;
+                return [numNotes, inversionSplit];
+            default:
+                throw new Error(`Unsupported chord difficulty: ${difficulty}`);
+        }
+    }
+
+    private getEasyChord(): Chord {
+        const numUninvertedTriads: number = ChordInterval.uninvertedTriadIntervals.length;
+        const randomIndex: number = Math.floor(Math.random() * numUninvertedTriads);
+        const randomInterval: Interval[] = ChordInterval.uninvertedTriadIntervals[randomIndex];
+        const baseNoteNumber: number = 60;
+        const notes: Note[] = randomInterval.map((interval: Interval): Note => {
+            const noteNumber: number = baseNoteNumber + interval.semitones;
+            return Note.fromMidiNumber(noteNumber);
+        });
+        const chord: Chord = new Chord(notes);
+        return chord;
+    }
 
     static generateRandomChords(count: number): Chord[] {
         return [];
